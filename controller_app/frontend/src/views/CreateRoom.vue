d64223a4226b819e43bdc93626ef4d83014bd614
canhigher <wrcyksl@gmail.com>
2021-03-17 22:29:38 +0300

    updated frontend

--- controller_app/frontend/src/views/CreateRoom.vue
+++ controller_app/frontend/src/views/CreateRoom.vue
@@ -1,14 +1,19 @@
 <template>
 
     <b-card title="Create a room" sub-title="Guest control of playback state">
-      
+
         <b-form-group label="Individual radios">
           <b-form-radio name="some-radios" value="A">Play/Pause</b-form-radio>
           <b-form-radio name="some-radios" value="B">No Control</b-form-radio>
+          <b-form-spinbutton id="sb-inline" min = "1" v-model="value" inline style="margin: 20px;"></b-form-spinbutton>
         </b-form-group>
+        <div class="btn-controller">
+          <b-button to= "/create" variant="primary">Create</b-button>
+        </div>
+        <div class="btn-controller">
+          <b-button to= "/" variant="warning">Back</b-button>
+        </div>
 
-        <a href="#" class="card-link">Create a Room!</a>
-      <b-link href="/" class="card-link">Back</b-link>
     </b-card>
 
 </template>
@@ -16,8 +21,10 @@
 <script>
 export default {
   name: "CreateRoom",
-  props: {
-    msg: String
+  data() {
+    return {
+      value: 1
+    }
   }
 };
 </script>
@@ -35,7 +42,7 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
+.btn-controller {
+  margin: 5px;
 }
 </style>
